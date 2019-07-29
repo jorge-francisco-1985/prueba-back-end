10936a662c408b8ccfbae7c46572358497568af1
jorge-francisco-1985 <jorge.francisco@solfine.mx>
2019-07-29 16:32:36 -0500

    reparando

---
+++ build/precache-manifest.367406859be2465cd5e4f158ea68e3bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d99020996c879d9f4d7bcb66a0a3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6e717e639f08622642",
+    "url": "/static/css/main.649906ac.chunk.css"
+  },
+  {
+    "revision": "64c7f7662034a2b29da1",
+    "url": "/static/js/2.62240829.chunk.js"
+  },
+  {
+    "revision": "7e6e717e639f08622642",
+    "url": "/static/js/main.6435145a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
